ac47d949778019ec556d9aecae964637bcd73ae5
ksbains <k1bains@ucsd.edu>
2021-04-13 22:58:02 -0700

    Added some more routes for the RaspberryPi portion. The logic is not super sound, but that is alright as we will be doing different auth once the DB is connected

--- routes.js
+++ routes.js
@@ -1,4 +1,43 @@
 'use strict';
+function storeData (data) {
+    console.log("the data to store is: ");
+    console.log(data)
+    //STORE DATA TO-DO!!!!
+}
+
+function userInDB(user){
+    const username = user.username
+    console.log("The username is: " + username)
+    //this will be replaced with DB checks
+    var users = ["VOID", "alice", "bob", "charlie"]
+    for(var i = 0; i < users.length; i++){
+        if(username == users[i]){
+            return i;
+        }
+    }
+    return false;
+}
+function passwordInDB(user){
+    console.log("The password is: " + user.password)
+    var passwords = ["VOID","alice123", "bobISbob", "floaTruth"]
+    for(var i = 0; i < passwords.length; i++){
+        if(user.password == passwords[i] && i == userInDB(user)){
+            return true;
+        }
+    }
+    return false;
+}
+function checkPassword(user){
+    if(userInDB(user)){
+        if(passwordInDB(user)){
+            return "Wowo the user is certified FRESH";
+        } else {
+            return "hey buddy! You may be a user, but your password is WRONG!";
+        }
+    } else {
+        return "hey you! I don't take to kindly to strangers, and you aint no user of mine I did ever see!!";
+    }
+}
 module.exports = function( app ) {
 
 //HERE ARE ALL THE ROUTES
@@ -13,7 +52,27 @@ module.exports = function( app ) {
 
 
 //RASPBERRY ROUTES HERE
+    app.post('/pmData', (req,res) => {
+        //in this lambda, the request will have the pm 2.5 value, pm 10 value, time stamp, and other sensor data
+        var data = {
+                pm2_5: parseFloat(req.body.pm2_5),
+                pm10: parseFloat(req.body.pm10),
+                time: req.body.time
+        }
+        storeData(data)
+        res.send("your data has been recieved, if you see it logged")
+    });
 
+
+    app.post('/userCredentials', (req,res) => {
+        console.log(req.body)
+        var data = {
+                username: req.body.username,
+                password: req.body.password
+        }
+        var response = checkPassword(data)
+        res.send(response)
+    })
 
 
 //FRONT END ROUTES HERE
